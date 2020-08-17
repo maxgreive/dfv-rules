ab3a16aef41fe7d4075fd5c2a79c71a84ee4b22a
Max Greive <max.greive@golfpost.de>
2020-08-17 21:04:49 +0200

    slug function in algolia.js

--- src/@rocketseat/gatsby-theme-docs/util/algolia.js
+++ src/@rocketseat/gatsby-theme-docs/util/algolia.js
@@ -1,5 +1,3 @@
-import slug from './slug';
-
 const docsQuery = `
 	{
 		allMdx {
@@ -79,5 +77,18 @@ const queries = [
 		},
 	},
 ]
+
+function slug(string) {
+	return string
+		.toString() // Cast to string
+		.toLowerCase() // Convert the string to lowercase letters
+		.trim() // Remove whitespace from both sides of a string
+		.replace(/\s/g, '-') // Replace each space with -
+		.replace(
+			/[^\w\-\u00b4\u00C0-\u00C4\u00c7\u00C9-\u00CA\u00CD\u00D3-\u00D6\u00DA\u00DC\u00E0-\u00E4\u00E7\u00E9-\u00EA\u00ED\u00F3-\u00F6\u00FA\u00FC]+/g,
+			'',
+		); // Removes all chars that aren't words, -, ´ or some latin characters (À Á Â Ã Ç É Ê Í Ó Ô Õ Ú à á â ã ç é ê í ó ô õ ú)
+}
+
 
 module.exports = queries
